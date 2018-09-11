f005f5ae797a86794372109ed25d82ddd299a183
Nathan Bakhuijzen <nathanbakhuijzen@gmail.com>
2018-09-11 16:03:59 +0200

    Add rocket module

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
+#![feature(plugin)]
+#![plugin(rocket_codegen)]
+
+extern crate rocket;
+
 pub(crate) mod beautifier;
 pub(crate) mod core;
 pub(crate) mod gesture;
 pub(crate) mod sensor;
 pub(crate) mod store;
 pub(crate) mod types;
+pub(crate) mod web;
 
 use core::Core;
 
