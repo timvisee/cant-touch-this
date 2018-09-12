1eebb3aa81c15c380a3359fa781443f3c7c06fc6
timvisee <timvisee@gmail.com>
2018-09-12 12:31:26 +0200

    Create sensor listener struct

--- src/sensor/controller.rs
+++ src/sensor/controller.rs
@@ -1,11 +1,18 @@
+use super::sensor::{Sensor, SensorListener};
+
 /// This thing should manage controllers, fethcing points
-pub struct SensorController {}
+pub struct SensorController {
+    /// The leap motion sensor.
+    ///
+    /// This will be extended in the future for supporting multiple sensors
+    sensor: Sensor,
+}
 
 impl SensorController {
     /// Construct a sensor controller.
     pub fn new() -> Self {
-        Self {}
+        Self {
+            sensor: Sensor::new(SensorListener::new()),
+        }
     }
 }
-
-// TODO: sensor struct
--- src/sensor/sensor.rs
+++ src/sensor/sensor.rs
@@ -1,4 +1,7 @@
-use leap::Controller as LeapController;
+use leap::{
+    Controller as LeapController,
+    Listener as LeapListener,
+};
 
 /// Structure representing a motion sensor.
 pub struct Sensor {
@@ -7,9 +10,33 @@ pub struct Sensor {
 
 impl Sensor {
     /// Construct a new sensor.
-    pub fn new() -> Self {
+    pub fn new(listener: SensorListener) -> Self {
         Self {
-            controller: LeapController::new(),
+            controller: LeapController::with_listener(listener),
         }
+    }
+}
+
+/// A sensor listener.
+pub struct SensorListener {}
+
+impl SensorListener {
+    /// Construct a new sensor listener.
+    pub fn new() -> Self {
+        Self {}
+    }
+}
+
+impl LeapListener for SensorListener {
+    fn on_frame(&mut self, controller: &LeapController) {
+        println!("Got update from Leap Motion sensor: {} FPS", controller.frame().current_fps());
+    }
+
+    fn on_connect(&mut self, _: &LeapController) {
+        println!("Leap Motion sensor connected");
+    }
+
+    fn on_disconnect(&mut self, _: &LeapController) {
+        println!("Leap Motion sensor disconnected");
     }
 }
