84123b1b4bf7807201452eb2d5f386403b89870e
timvisee <timvisee@gmail.com>
2018-09-27 11:58:51 +0200

    Fix build issue introduced in merge, remove redundant crate import

--- src/web/server.rs
+++ src/web/server.rs
@@ -4,7 +4,7 @@
 use std::{collections::HashMap, sync::Arc};
 
 use rocket::{self, State};
-use rocket_contrib::{static_files::staticfiles, json, template};
+use rocket_contrib::{static_files::StaticFiles, Json, Template};
 
 use gesture::GestureController;
 
