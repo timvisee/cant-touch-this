f111e7be29b46967df5d72256b90cbccd2e1d551
timvisee <timvisee@gmail.com>
2018-09-27 13:32:17 +0200

    Show recording state on record button

--- res/static/js/app.js
+++ res/static/js/app.js
@@ -1,9 +1,29 @@
 $('#start_recording').on('click', function() {
-    axios.get('/api/v1/record/true')
+    let recording = $(this).hasClass("btn-danger");
+
+    axios.get('/api/v1/record/' + !recording)
         .then(function (response) {
             console.log(response);
+            setRecordingState(response.data.started);
         })
         .catch(function (error) {
             console.log(error);
         });
 });
+
+/**
+ * Set the recording state.
+ */
+function setRecordingState(recording) {
+    let button = $('#start_recording');
+
+    if(recording) {
+        button.text("Recording...");
+        button.removeClass("btn-outline-success");
+        button.addClass("btn-danger");
+    } else {
+        button.text("Start recording");
+        button.removeClass("btn-danger");
+        button.addClass("btn-outline-success");
+    }
+}
