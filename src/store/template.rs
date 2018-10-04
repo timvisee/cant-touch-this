cf60df69006e760269d7364477cb64e6e39b3bd5
timvisee <timvisee@gmail.com>
2018-10-04 14:38:14 +0200

    Reformat using rustfmt

--- src/store/template.rs
+++ src/store/template.rs
@@ -121,66 +121,63 @@ impl TemplateStore {
             .expect("failed to lock templates list for detecting gestures");
 
         // Find a matching template for the current trace
-        let hit = templates
-            .par_iter()
-            .find_any(|template| {
-                // Get the model to compare against
-                let model = template.model();
+        let hit = templates.par_iter().find_any(|template| {
+            // Get the model to compare against
+            let model = template.model();
 
-                // Get the model and other model points
-                let model_points = model.trace().points();
-                let other_points = other.model().trace().points();
-                let model_count = model_points.len();
-                let other_count = other_points.len();
+            // Get the model and other model points
+            let model_points = model.trace().points();
+            let other_points = other.model().trace().points();
+            let model_count = model_points.len();
+            let other_count = other_points.len();
 
-                // Skip if the template has more points than our current trace
-                if other_count < model_count {
-                    return false;
-                }
+            // Skip if the template has more points than our current trace
+            if other_count < model_count {
+                return false;
+            }
 
-                // Determine how many points to process, minimum length wins
-                let count = min(model_points.len(), other_points.len());
+            // Determine how many points to process, minimum length wins
+            let count = min(model_points.len(), other_points.len());
 
-                // Select the last points based on the determined count to use
-                let model_points = &model.trace().points()[model_count - count..model_count];
-                let other_points =
-                    &other.model().trace().points()[other_count - count..other_count];
+            // Select the last points based on the determined count to use
+            let model_points = &model.trace().points()[model_count - count..model_count];
+            let other_points = &other.model().trace().points()[other_count - count..other_count];
 
-                // Caluclate the difference for each point
-                let diff = model_points
-                    .iter()
-                    .rev()
-                    .zip(other_points.iter().rev())
-                    .map(|(a, b)| rad_diff(b.radians(), a.radians()));
+            // Caluclate the difference for each point
+            let diff = model_points
+                .iter()
+                .rev()
+                .zip(other_points.iter().rev())
+                .map(|(a, b)| rad_diff(b.radians(), a.radians()));
 
-                // Calculate the cummulative difference on each point
-                let cum_diff: Vec<f64> = diff
-                    .scan(0.0, |acc, p| {
-                        *acc += p;
-                        Some(*acc)
-                    })
-                    .collect();
+            // Calculate the cummulative difference on each point
+            let cum_diff: Vec<f64> = diff
+                .scan(0.0, |acc, p| {
+                    *acc += p;
+                    Some(*acc)
+                })
+                .collect();
 
-                // Skip if the total difference is too big
-                if cum_diff.last().unwrap().abs() > TOTAL_DIFF_MAX {
-                    return false;
-                }
+            // Skip if the total difference is too big
+            if cum_diff.last().unwrap().abs() > TOTAL_DIFF_MAX {
+                return false;
+            }
 
-                // Skip if any of the points has a difference of more than 2
-                if cum_diff.iter().any(|p| p.abs() > POINT_DIFF_MAX) {
-                    return false;
-                }
+            // Skip if any of the points has a difference of more than 2
+            if cum_diff.iter().any(|p| p.abs() > POINT_DIFF_MAX) {
+                return false;
+            }
 
-                // Skip if each window of 5 points has an average difference bigger than 1
-                if GROUP_SIZE > 0 && cum_diff
-                    .windows(GROUP_SIZE)
-                    .any(|p| (p.iter().sum::<f64>().abs() / GROUP_SIZE as f64) > GROUP_DIFF_MAX)
-                {
-                    return false;
-                }
+            // Skip if each window of 5 points has an average difference bigger than 1
+            if GROUP_SIZE > 0 && cum_diff
+                .windows(GROUP_SIZE)
+                .any(|p| (p.iter().sum::<f64>().abs() / GROUP_SIZE as f64) > GROUP_DIFF_MAX)
+            {
+                return false;
+            }
 
-                true
-            });
+            true
+        });
 
         if let Some(hit) = hit {
             // Clear the fragment history
